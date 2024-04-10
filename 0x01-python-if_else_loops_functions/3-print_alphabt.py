8e543025ccc438356639ee32cf670f8762a0e7fd
STARR44 <okekeesther100@gmail.com>
2024-04-10 02:34:36 -0700

    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for b in range(97, 123):
-    if (b != 113) and (b != 101):
-        print(f"{chr(b)}", end="")
+    if b in [113, 101]:
+        continue
+    print(f"{chr(b)}", end="")
