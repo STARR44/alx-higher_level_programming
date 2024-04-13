13715166d49ced912fa39d111c2b6e93b5824fb3
STARR44 <okekeesther100@gmail.com>
2024-04-13 00:44:11 -0700

    a program that prints uppercase characters

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,7 @@ def uppercase(str):
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
             temp = chr(ord(char) - 32)
-        else: temp = char
+        else: 
+            temp = char
         print("{}" .format(temp), end="")
     print('')
