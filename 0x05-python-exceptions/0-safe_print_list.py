3337b71f44a4bc9f5a01238d65c849cda3bdd911
STARR44 <okekeesther100@gmail.com>
2024-05-03 12:54:55 -0700

    A program that prints x elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    no_count = 0
     try:
-        for i, element in enumerate(my_list):
-            if no_count < x:
+        for index, element in enumerate(my_list):
+            if index < x:
                 print("{}".format(element), end="")
-                no_count += 1
-    except Exception:
+    except IndexError:
         pass
     print()
-    return(no_count)
+    return(index) + 1
