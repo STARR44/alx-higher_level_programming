2f4a59ad1f49d304e221ecf756c8268d817a9c77
STARR44 <okekeesther100@gmail.com>
2024-05-03 21:41:37 -0700

    Aprogram that prints x elements of a list and returns the count of the real numbers printed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     index = 0
-    count = 0
     try:
         for index, element in enumerate(my_list):
             if index == x:
                 break
             print("{}".format(element), end="")
-            count += 1
+            index += 1
     except IndexError:
         pass
     print()
-    return count
+    return index
