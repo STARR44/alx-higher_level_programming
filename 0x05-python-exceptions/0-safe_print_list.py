c6cd50b425efbfd0b83790e88ade9ab6c4a6218b
STARR44 <okekeesther100@gmail.com>
2024-05-05 04:16:10 -0700

    A program that prints the first x elements and only integers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    index = 0
-    try:
-        for index, element in enumerate(my_list):
-            if index == x:
-                break
-            print("{}".format(element), end="")
-            index += 1
-    except IndexError:
-        pass
-    print()
-    return index
+    count = 0
+    for index in range(my_list[x:])
+        try:
+            print("{}".format(my_list[index]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        print()
+        return count
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for p, element in enumerate(my_list):
-        if count < x:
-            try:
-                print("{:d}".format(element), end="")
-                count += 1
-            except (ValueError, TypeError):
-                pass
-            except IndexError:
-                raise
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
     print()
     return count
